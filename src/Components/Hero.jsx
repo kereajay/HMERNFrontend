c004b20221e663082664e9930e4392d0f2ba2cae
Ajay <kereajay578@gmail.com>
2024-10-13 12:27:22 +0530

    updated ui

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero({title,imageurl}) {
   return (
     <div className='grid lg:grid-cols-2 mt-0'>
-        <div className='px-20'>
+        <div className='px-20 mt-24'>
             <h1 className='text-3xl font-bold text-gray-500 mt-10'>{title}</h1>
             <br />
             <p className='text-lg text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque est quas nam, laborum sunt fugit repellat illum labore odio porro corporis hic quibusdam architecto, dignissimos cupiditate adipisci quisquam minima. Itaque.</p>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -89,7 +89,7 @@ function Navbar() {
         )}
 
         <div className="px-4 flex flex-row items-center gap-5 justify-end">
-          <p onClick={handletheme}>
+          <p onClick={handletheme} className="hover:cursor-pointer">
             <IoSunnySharp />
           </p>
           {isAuthenticated === true ? (
@@ -100,17 +100,25 @@ function Navbar() {
               Logout
             </button>
           ) : (
-            <button
-              className="text-xl font-semibold bg-blue-400 py-1 px-2 rounded-2xl text-white"
-              onClick={handlelogin}
-            >
+            <button class="bg-blue-300 text-lg text-black border border-blue-400 border-b-4 font-bold overflow-hidden relative px-4 py-2 rounded-3xl hover:brightness-10 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"  onClick={handlelogin}>
+              <span class="bg-blue-400 shadow-blue-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
               Login
             </button>
           )}
           <Link to="https://hmern-dashboard.vercel.app/">
             {" "}
-            <button className="text-xl font-semibold bg-blue-400 py-1 px-2 rounded-2xl text-white">
+            <button class="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-blue-300 backdrop-blur-md lg:font-bold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group">
               Admin
+              <svg
+                class="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+                viewBox="0 0 16 19"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                  class="fill-gray-800 group-hover:fill-gray-800"
+                ></path>
+              </svg>
             </button>
           </Link>
         </div>
@@ -138,7 +146,7 @@ function Navbar() {
                 <li>
                   <Link to="/about">AboutUs</Link>
                 </li>
-                <li onClick={handletheme}>
+                <li onClick={handletheme} className="hover:cursor-pointer">
                   <IoSunnySharp />
                 </li>{" "}
               </ul>
